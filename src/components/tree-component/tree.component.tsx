4165f10a91186ed5d488b87578535b32605f0558
Andrey <asbogdanov13@gmail.com>
2022-03-10 09:46:21 +0500

    remove console.log from component

--- src/components/tree-component/tree.component.tsx
+++ src/components/tree-component/tree.component.tsx
@@ -1,34 +1,33 @@
 import * as React from "react";
 import "./tree.styles.css";
 import pathShapes, { straight } from "./pathShapes";
-import 
-  { processTree, 
-    createNodesArray, 
-    createConnectingLinesArray,
-  } 
-from "./treeBuildFunctions";
-import 
-  {
-    PathFunction,
-    TreeElementWithCoords,
-    TreeProps,
-    Path
-  } 
-from "./interfaces";
+import {
+processTree,
+createNodesArray,
+createConnectingLinesArray,
+}
+  from "./treeBuildFunctions";
+import {
+PathFunction,
+TreeElementWithCoords,
+TreeProps,
+Path
+}
+  from "./interfaces";
 
 export class Tree extends React.Component<TreeProps> {
-    /**
-    * @class Tree 
-    * builds tree graph from user's object. Graph exist is rectangles(nodes) and connecting lines. 
-    * @param {TreeElement} data - source user's object.
-    * @param {number} nodeWidth - width of the rectangle(node) in pixels, that user's content is placed in. Not mandatory. 100 by default.
-    * @param {number} nodeHeight - height of the node in pixels. 50 by default.
-    * @param {number | function} xOffset -  x distance between adjacent two nodes. By default 50 pixels. Also, it may be a custom function.
-    * @param {PathShape | PathFunction} pathShape - funcion, calculates svg lines between two nodes. There are three offered functions. bezier by default. Also, it may be a custom function.
-    * @param {function} nodeContent - user's function, returns HTML element, wich will be placed into node rectangle.
-    * @param {string} lineClassName - connecting lines className. Located in tree.style.css. By default "connectingLine".
-    * @returns {HTMLElement} renders tree
-    */
+  /**
+  * @class Tree 
+  * builds tree graph from user's object. Graph exist is rectangles(nodes) and connecting lines. 
+  * @param {TreeElement} data - source user's object.
+  * @param {number} nodeWidth - width of the rectangle(node) in pixels, that user's content is placed in. Not mandatory. 100 by default.
+  * @param {number} nodeHeight - height of the node in pixels. 50 by default.
+  * @param {number | function} xOffset -  x distance between adjacent two nodes. By default 50 pixels. Also, it may be a custom function.
+  * @param {PathShape | PathFunction} pathShape - funcion, calculates svg lines between two nodes. There are three offered functions. bezier by default. Also, it may be a custom function.
+  * @param {function} nodeContent - user's function, returns HTML element, wich will be placed into node rectangle.
+  * @param {string} lineClassName - connecting lines className. Located in tree.style.css. By default "connectingLine".
+  * @returns {HTMLElement} renders tree
+  */
   static defaultProps: Pick<TreeProps, "pathShape" | "nodeWidth" | "nodeHeight" | "lineClassName" | "xOffset" | "yOffset"> = {
     pathShape: pathShapes.bezier,
     nodeWidth: 100,
@@ -37,7 +36,7 @@ export class Tree extends React.Component<TreeProps> {
     xOffset: 50,
     yOffset: 50,
   };
-  
+
   render(): React.ReactElement {
     const {
       nodeWidth,
@@ -50,14 +49,12 @@ export class Tree extends React.Component<TreeProps> {
     }: TreeProps = this.props;
 
     const pathStyle: PathFunction =
-    typeof this.props.pathShape == "function" ? this.props.pathShape : pathShapes[this.props.pathShape];
+      typeof this.props.pathShape == "function" ? this.props.pathShape : pathShapes[this.props.pathShape];
 
-    console.log(this.props.pathShape);
-    
     const dataTree: TreeElementWithCoords = processTree(data, yOffset, xOffset, nodeWidth, nodeHeight);
 
     const dataList: TreeElementWithCoords[] = createNodesArray(dataTree);
-    
+
     const connectingLines: Path[] = createConnectingLinesArray(dataTree, nodeWidth, nodeHeight, pathStyle);
 
     const { width, height }: { width: number; height: number } = dataList.reduce(
@@ -67,7 +64,7 @@ export class Tree extends React.Component<TreeProps> {
       }),
       { width: 0, height: 0 }
     );
-    
+
     return (
       <div>
         <div className="root" style={{ width: width, height: height }}>
